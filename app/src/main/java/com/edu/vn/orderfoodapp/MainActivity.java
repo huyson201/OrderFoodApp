f0467d596a15ff6e183107943e66f65d1da8eff6
HuySon <huyson201@gmail.com>
2021-05-08 21:23:34 +0700

    implement lb

--- app/src/main/java/com/edu/vn/orderfoodapp/MainActivity.java
+++ app/src/main/java/com/edu/vn/orderfoodapp/MainActivity.java
@@ -3,6 +3,11 @@ package com.edu.vn.orderfoodapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
