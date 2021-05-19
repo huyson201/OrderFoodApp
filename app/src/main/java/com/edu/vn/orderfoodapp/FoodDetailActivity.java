4f897025ebe790c63f57589d58eaf38e9df098e2
HuySon <huyson201@gmail.com>
2021-05-19 21:09:01 +0700

    add detail activity

---
+++ app/src/main/java/com/edu/vn/orderfoodapp/FoodDetailActivity.java
@@ -0,0 +1,14 @@
+package com.edu.vn.orderfoodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.food_detail_layout);
+    }
+}
--- app/src/main/java/com/edu/vn/orderfoodapp/models/Invoice.java
+++ app/src/main/java/com/edu/vn/orderfoodapp/models/Invoice.java
@@ -39,4 +39,6 @@ public class Invoice {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
+
+    
 }
