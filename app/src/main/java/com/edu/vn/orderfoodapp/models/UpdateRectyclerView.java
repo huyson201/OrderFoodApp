14cf479867db5f800d6109992685cd3cfa0aaf34
webcms <phbaongoc2001@gmail.com>
2021-05-25 20:36:13 +0700

    fix category

--- app/src/main/java/com/edu/vn/orderfoodapp/models/UpdateRectyclerView.java
+++
@@ -1,5 +0,0 @@
-package com.edu.vn.orderfoodapp.models;
-
-public interface UpdateRectyclerView {
-
-}
---
+++ app/src/main/java/com/edu/vn/orderfoodapp/models/UpdateRecyclerView.java
@@ -0,0 +1,7 @@
+package com.edu.vn.orderfoodapp.models;
+
+import java.util.ArrayList;
+
+public interface UpdateRectyclerView {
+ void callBack(int position, ArrayList<Food> foods);
+}
